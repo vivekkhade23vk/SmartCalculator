5f269c12df80d0822752f23c3897af21970f3ea1
vivek khade <vivekkhade23vk@gmail.com>
2024-10-12 17:57:19 +0530

    extra features Substract,multiply,division

--- src/SmartCalculator.js
+++ src/SmartCalculator.js
@@ -2,16 +2,40 @@
 
 class SmartCalculator {
     add(numbers) {
-        if(numbers=="") return 0;
-        let numArr=numbers.split(",").map(Number)
-        let sum=0;
-        for(let i=0;i<numArr.length;i++){
-            sum+=numArr[i]
+        if (numbers == "") return 0;
+        let numArr = numbers.split(",").map(Number)
+        let sum = 0;
+        for (let i = 0; i < numArr.length; i++) {
+            sum += numArr[i]
         }
         return sum;
-        
+
     }
-  }
-  
-  module.exports = SmartCalculator;
-  
+    multiply(numbers) {
+        if (numbers == ""||numbers==0) return 0;
+        let numArr = numbers.split(",").map(Number)
+        let mul = 1;
+        for (let i = 0; i < numArr.length; i++) {
+            mul *= numArr[i]
+        }
+        return mul;
+
+    }
+    substract(numbers) {
+        if (numbers == ""||numbers==0) return 0;
+        let numArr = numbers.split(",").map(Number)
+     
+        return numArr[0]-numArr[1];
+
+    }
+
+    division(numbers) {
+        if (numbers == "") return 0;
+        let numArr = numbers.split(",").map(Number)
+      
+        return numArr[0]/numArr[1];
+
+    }
+}
+
+module.exports = SmartCalculator;
--- tests/SmartCalculator.test.js
+++ tests/SmartCalculator.test.js
@@ -6,23 +6,83 @@ const SmartCalculator = require('../src/SmartCalculator');
 // test case for empty string
 
 describe('String Calculator', () => {
-  test('it should return 0 for an empty("") string', () => {
-    const calculator = new SmartCalculator();
-    expect(calculator.add("")).toBe(0);
-  });
+    test('it should return 0 for an empty("") string', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.add("")).toBe(0);
+    });
 });
 
+// test case for single Number string
 
 describe('String Calculator for Single Number', () => {
     test('should return 1 when passed 1', () => {
-      const calculator = new SmartCalculator();
-      expect(calculator.add("1")).toBe(1);
+        const calculator = new SmartCalculator();
+        expect(calculator.add("1")).toBe(1);
     });
-  });
+});
 
-  describe('String Calculator for multiple Number', () => {
+// test case for multiple Number string
+describe('String Calculator for multiple Number', () => {
     test('should return 6 when passed 1,5', () => {
-      const calculator = new SmartCalculator();
-      expect(calculator.add("1,5")).toBe(6);
+        const calculator = new SmartCalculator();
+        expect(calculator.add("1,5")).toBe(6);
     });
-  });
+});
+
+// test case for empty string
+describe('String Calculator', () => {
+    test('it should return 0 for an empty("") string or 0', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.multiply("")).toBe(0);
+        expect(calculator.multiply(0)).toBe(0);
+
+    });
+});
+
+// test case for single Number string
+describe('String Calculator for Single Number', () => {
+    test('should return 1 when passed 1', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.multiply("1")).toBe(1);
+    });
+});
+
+// test case for multiple Number string
+describe('String Calculator for multiple Number', () => {
+    test('should return 6 when passed 1,5', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.multiply("1,5")).toBe(5);
+    });
+});
+
+// test case for empty string
+describe('String Calculator', () => {
+    test('it should return 0 for an empty("") string', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.substract("")).toBe(0);
+    });
+});
+
+// test case for multiple Number string
+describe('String Calculator for multiple Number', () => {
+    test('should return 5 when passed 10,5', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.substract("10,5")).toBe(5);
+    });
+});
+
+// test case for empty string
+describe('String Calculator', () => {
+    test('it should return 0 for an empty("") string', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.division("")).toBe(0);
+    });
+});
+
+// test case for multiple Number string
+describe('String Calculator for multiple Number', () => {
+    test('should return 2 when passed 10,5', () => {
+        const calculator = new SmartCalculator();
+        expect(calculator.division("10,5")).toBe(2);
+    });
+});
